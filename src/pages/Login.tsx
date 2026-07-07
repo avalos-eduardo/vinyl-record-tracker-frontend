9481c3c2996a58183a193e024f38c0079f94f25e
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2026-07-06 23:50:48 -0500

    fixed login to navigate to /dashboard

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -36,7 +36,7 @@ export default function Login() {
       const success = await login();
 
       if (success) {
-        navigate("/home");
+        navigate("/dashboard");
       }
     } catch (err) {
       setError("Something went wrong, please try again.");
